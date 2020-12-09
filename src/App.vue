b44c7cacb46fa1dbbaf0dbcc849a7df91cdc9d76
armandupe <symbolic28@gmail.com>
2020-12-09 10:03:38 +0300

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,30 @@
     <div id="nav">
       <router-link class="nav__link" to="/">Home</router-link> |
       <router-link class="nav__link" to="/albums2018">albums 2018</router-link>
-      <router-link class="nav__link" to="/russian2018">russian albums 2018</router-link>
+      <router-link class="nav__link" to="/russian2018"
+        >russian albums 2018</router-link
+      >
       <router-link class="nav__link" to="/ep2018">ep 2018</router-link>
-      <router-link class="nav__link" to="/albums2019">albums 2019</router-link> |
+      <router-link class="nav__link" to="/albums2019">albums 2019</router-link>
+      |
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-}
+  name: "app",
+};
 </script>
 
 <style lang="scss">
-
 body {
-  font-family: 'Oswald', sans-serif;
+  font-family: "Oswald", sans-serif;
   background-color: #000;
   margin: 0;
   overflow-x: hidden;
-  -ms-overflow-style: none;  
+  -ms-overflow-style: none;
   scrollbar-width: none;
 }
 
@@ -35,87 +37,8 @@ body::-webkit-scrollbar {
 .nav__link {
   text-decoration: none;
   &:hover {
-    opacity: .8;
+    opacity: 0.8;
   }
-}
-
-iframe {
-  border: none;
-  width: 100%;
-  height: 450px;
-  position: fixed;
-  z-index: 70;
-  top: 25%;
-  left: 0%;
-  box-shadow: 1px 1px 15px 5px #42b983;
-  @media (min-width: 600px) {
-    height: 450px;
-  } 
-  @media (min-width: 768px) {
-    width: 450px;
-    left: 35%;
-  }   
-}
-
-.close-button {
-  position: fixed;
-  // width: 25px;
-  // height: 25px;
-  // top: 16%;
-  // left: 59%;
-  padding-top: 10rem;
-  width: 3100px;
-  height: 1000px;
-  top: 0;
-  left: 0;
-  font-size: 4.5rem;
-  z-index: 66;
-  cursor: pointer;
-  &:hover {
-    color: #42b983;
-  }
-}
-
-.template-container {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  @media (min-width: 768px) {
-    flex-direction: row;
-  }   
-  
-}
-
-.artwork {
-  max-width: 100%;
-  height: auto;
-  transition: .3s;
-  cursor: pointer;
-  &:hover {
-    opacity: .7;
-  }
-}
-
-.filter-blur {
-  filter: blur(3px);
-}
-
-.album {
-  width: 100%;
-  @media (min-width: 768px) {
-    width: 25%;
-  }  
-  &__title::selection {
-    color: #42b983;
-  }
-}
-
-.hideFrame {
-  display: none;
-}
-
-.showFrame {
-  display: block;
 }
 
 #app {
@@ -136,5 +59,4 @@ iframe {
     }
   }
 }
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,50 @@
 <template>
-    <div class="home">
-        <ul class="home__list">
-            <router-link class="home__list-link" to="/albums2019">2019</router-link>
-            <router-link class="home__list-link" to="/albums2018">2018</router-link>
-            <router-link class="home__list-link" to="/russian2018">russian 2018</router-link>
-            <router-link class="home__list-link" to="/ep2018">EP 2018</router-link>
-        </ul>
-    </div>
+  <div class="home">
+    <ul class="home__list">
+      <router-link class="home__list-link" to="/albums2019">2019</router-link>
+      <router-link class="home__list-link" to="/albums2018">2018</router-link>
+      <router-link class="home__list-link" to="/russian2018">russian 2018</router-link>
+      <router-link class="home__list-link" to="/ep2018">EP 2018</router-link>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss">
 .home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  &__list {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    &__list {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  &__list-link {
+    color: #42b983;
+    font-size: 6rem;
+    text-decoration: none;
+    transition: 0.3s;
+    margin-right: 3rem;
+    &:hover {
+      text-decoration: underline;
     }
-    &__list-link {
-        color: #42b983;
-        font-size: 6rem;
-        text-decoration: none;
-        transition: .3s;
-        margin-right: 3rem;
-        &:hover {
-            text-decoration: underline;     
-        }
-    }
+  }
 }
 </style>
 
 <script>
 export default {
-    mounted() {
-        const nav = document.getElementById('nav');
-        const home = document.querySelector('.home');
-        const links = document.querySelectorAll('.home__list-link');
-        
-        if (home) nav.classList.add('hideFrame');
-        links.forEach(link => link.addEventListener('click', () => nav.classList.remove('hideFrame')));
-    }
-}
-</script>>
+  mounted() {
+    const nav = document.getElementById("nav");
+    const home = document.querySelector(".home");
+    const links = document.querySelectorAll(".home__list-link");
+
+    if (home) nav.classList.add("hideFrame");
+    links.forEach((link) =>
+      link.addEventListener("click", () => nav.classList.remove("hideFrame"))
+    );
+  },
+};
+</script>
